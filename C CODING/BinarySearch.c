0f707a7ca43446a4fe62c5ead0561804c7adb3ee
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-03-23 17:20:46 +0530

    Add binary funtion

--- C CODING/BinarySearch.c
+++ C CODING/BinarySearch.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 
-int BinarySearch(){
+int BinarySearch(int arr[],int key,int left,int right){
+
+     int mid = left + (right - left) / 2;
+
+     while (left <= right)
+     {
+       if(arr[mid]== key){
+        return mid;
+       }
+       if(key > arr[mid]){
+        left = mid + 1;
+       }
+       if(key < arr[mid]){
+        right = mid - 1;
+       }
+        return -1;
+     }
+     
 
 }
 int main(){
 
     int arr[12] = {12,23,45,67};
     int n = 4;
+    int key;
+
+    int result = BinarySearch(arr,45, 0, n - 1);
     return 0;
 }
