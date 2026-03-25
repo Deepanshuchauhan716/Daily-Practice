6a70b3e10d513a10c243f409fd2ddfc7961026d8
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-03-25 22:13:55 +0530

    Create function for B

--- C CODING/bubblesort.c
+++ C CODING/bubblesort.c
@@ -5,6 +5,19 @@ void OriginalArray(int arr[],int n){
     for(int i = 0; i < n; i++){
         printf("%d ",arr[i]);
     }
+    printf("\n");
+}
+
+void BubbleSort(int arr[],int n){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; i++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 int main(){
 
@@ -13,4 +26,5 @@ int main(){
 
     printf("original Array");
     OriginalArray(arr,n);
+    BubbleSort(arr,n);
 }
