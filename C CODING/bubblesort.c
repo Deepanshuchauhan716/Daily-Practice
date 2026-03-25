09e37c156d36af2b694d18e38d25dcebcb596cf4
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-03-25 22:10:11 +0530

    Create function for Print original array

--- C CODING/bubblesort.c
+++ C CODING/bubblesort.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+
+void OriginalArray(int arr[],int n){
+
+    for(int i = 0; i < n; i++){
+        printf("%d ",arr[i]);
+    }
+}
 int main(){
 
     int arr[] = {12,34,56,78,67};
     int n = sizeof(arr) / sizeof(arr[0]); 
+
+    printf("original Array");
+    OriginalArray(arr,n);
 }
