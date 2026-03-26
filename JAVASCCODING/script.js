c276da2c1f325249b9a05dceeb2446b73ad7a6ba
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-03-26 18:20:59 +0530

    Remove All code

--- JAVASCCODING/script.js
+++ JAVASCCODING/script.js
@@ -1,25 +1,2 @@
-// variable in java script
 
-// var in js
-// var me hum same naam se declare kr skte hai aur value bhi chnage kr skte hai
-var one = 1;
-var one = 2;
-console.log(one);
-
-// let variable in js
-// let me hum same naam ke variable ko 2 baar use nhi kr skte
-let three = 3;
-    three = 4;
-
-console.log(three);
-
-// lekin hum value change kr skte hai
-
-// const variable in js
-// const me hum na hi value assign kr skte hai aur na hi same name ke variable ko 2 baar use kr skte hai!
-
-const myname = "Deepanshu";
-      myname = "Dheeraj";
-
-console.log(myname);
 
