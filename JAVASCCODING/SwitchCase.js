0daf65d235bef06937b2177c549cb7bd2e756cc4
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-03-27 13:18:15 +0530

    Switch case in js

---
+++ JAVASCCODING/SwitchCase.js
@@ -0,0 +1,14 @@
+// Switch case in js
+
+let fruit = "Apple";
+
+switch(fruit){
+    case "Banana":
+        console.log("Yellow");
+        break;
+    case "Apple":
+        console.log("Red");
+        break;
+    default:
+        console.log("Unknown");
+}
