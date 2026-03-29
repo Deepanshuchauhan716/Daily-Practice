b20b566954765ed2c78eaa8d3606bd272a2b3bec
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-03-29 22:23:18 +0530

    Print all array elemnt in js

--- JAVASCCODING/array.js
+++ JAVASCCODING/array.js
@@ -1,10 +1,4 @@
-let arr = [12,34,56,78,87,65,43];  //find average
-
-let sum = 0;
-
-for(let val of arr){
-    sum = sum + val;
+let arr  = [12,34,56];
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
 }
-
-let avg = sum / arr.length;
-console.log(avg);
