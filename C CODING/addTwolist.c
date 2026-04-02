be690268030a564721dba495c6f169fec185ae8f
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-02 19:00:42 +0530

    Pending...

--- C CODING/addTwolist.c
+++ C CODING/addTwolist.c
@@ -5,3 +5,15 @@ struct node{
     int data;
     struct node * next;
 };
+
+int main(){
+   int num,x;
+
+   printf("Enter number of element : ");
+   scanf("%d ",&num);
+
+
+   printf("Enter element (enter -1 to stop)");
+   scanf("%d ",&x);
+
+}
